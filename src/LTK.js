dbf7cf4d99ac0fb575d89734422572245d9fddfa
kidnes <kidnes88@gmail.com>
2014-07-08 22:35:05 +0800

    Update LTK.js
    1、去除全局正则匹配；

--- src/LTK.js
+++ src/LTK.js
@@ -17,119 +17,164 @@
 
     var LTK = global.LTK = {
         version: "1.0.0"
-    }
+    };
 
     var mods = LTK.mods = {};
+    var data = LTK.data = {};
 
     function isType(type) {
         return function(obj) {
-            return {}.toString.call(obj) == "[object " + type + "]"
-        }
+            return {}.toString.call(obj) === "[object " + type + "]";
+        };
     }
 
-    var isArray = Array.isArray || isType("Array")
-    var isFunction = isType("Function")
+    var isArray = Array.isArray || isType("Array");
+    var isFunction = isType("Function");
+    var isString = isType("String");
+    var isObject = isType("Object");
 
-    var REQUIRE_RE = /"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|\/\*[\S\s]*?\*\/|\/(?:\\\/|[^\/\r\n])+\/(?=[^\/])|\/\/.*|\.\s*LTK\.require|(?:^|[^$])\bLTK\.require\s*\(\s*(["'])(.+?)\1\s*\)/g,
-        SLASH_RE = /\\\\/g,
-        IDEXEC_RE = /^\-([\w-]*)$/;
+    var SLASH_RE = /\\\\/g,
+        IDEXEC_RE = /^\-([\w\.\/\-]*)$/;
 
-    function parseDependencies(code) {
-        var ret = []
+    var DOT_RE = /\w+\.\//g;
+    var DOUBLE_DOT_RE = /\w+\.\w+\.\.\//;
 
-        code.replace(SLASH_RE, "")
-            .replace(REQUIRE_RE, function(m, m1, m2) {
-                if (m2) {
-                    ret.push(m2)
-                }
-            })
+    function parsePaths(id, path) {
+        if (!/^\./.test(id)) return id;
 
-        return ret
+        path = (path+id).replace(DOT_RE, "");
+
+        while (path.match(DOUBLE_DOT_RE)) {
+            path = path.replace(DOUBLE_DOT_RE, "");
+        }
+
+        return path;
+    }
+
+    function parseAlias(id) {
+        var alias = data.alias;
+        return alias && isString(alias[id]) ? alias[id] : id;
+    }
+
+    function parseId(id, path) {
+        if (!id) return "";
+
+        id = parseAlias(id);
+        id = parsePaths(id, path);
+
+        return id;
     }
 
     function exec(meta) {
         if (meta.exports !== null) return meta.exports;
-
-        if (isFunction(meta.factory)) {
-            meta.deps = meta.deps.concat(parseDependencies(meta.factory.toString()))
-        }
-
+		
+		for(var key in meta.deps){
+			exec(mods[meta.deps[key].id]);
+		};
+		/*
         meta.deps.forEach(function(id) {
-            exec(mods[id])
-        })
+            exec(mods[id]);
+        });
+		*/
 
         function require(id) {
-            return exec(mods[id])
+            id = parseId(id, meta.id);
+            if (!mods[id]) throw new Error(id +' not found.');
+            return exec(mods[id]);
         }
 
-        var factory = meta.factory
+        require.async = require;
+
+        var factory = meta.factory;
 
         var exports = isFunction(factory) ?
             factory(require, meta.exports = {}, meta) :
-            factory
+            factory;
 
         if (exports === undefined) {
-            exports = meta.exports
+            exports = meta.exports;
         }
 
-        meta.exports = exports
+        meta.exports = exports;
 
-        delete meta.factory
+        delete meta.factory;
 
         return exports;
     }
 
     LTK.define = function(id, deps, factory) {
-        var argsLen = arguments.length
+        var argsLen = arguments.length;
 
         if (argsLen === 1) {
-            throw "module must has a id and factory."
+            throw "module must has a id and factory.";
         } else if (argsLen === 2) {
-            factory = deps
+            factory = deps;
             deps = [];
         }
 
-        if (IDEXEC_RE.test(id)) id = RegExp.$1
+        if (IDEXEC_RE.test(id)) id = RegExp.$1;
 
-        if (mods[id]) {
+        /*if (mods[id]) {
             throw "module " + id + " has been defined.";
-        }
+        }*/
 
         var meta = {
             id: id,
             deps: deps,
             factory: factory,
             exports: null
-        }
+        };
 
-        mods[id] = meta
+        mods[id] = meta;
 
         RegExp.$1 == id && LTK.exec(id, true)
-    }
+    };
 
     LTK.exec = function(id) {
         return exec(mods[id]);
-    }
+    };
 
     LTK.use = function(ids, callback) {
-        !isArray(ids) && (ids = [ids])
+        if (!isArray(ids)) ids = [ids];
 
         var exports = [];
 
         for (var i = 0; i < ids.length; i++) {
-            exports[i] = LTK.exec(ids[i])
+            exports[i] = LTK.exec(ids[i]);
         }
 
         if (callback) {
-            callback.apply(global, exports)
+            callback.apply(global, exports);
         }
-    }
+    };
+
+    LTK.config = function(configData) {
+        for (var key in configData) {
+            var curr = configData[key];
+            var prev = data[key];
+
+            if (prev && isObject(prev)) {
+              for (var k in curr) {
+                prev[k] = curr[k];
+              }
+            }
+            else {
+              if (isArray(prev)) {
+                curr = prev.concat(curr);
+              }
+
+              data[key] = curr;
+            }
+        }
+
+        return LTK;
+    };
 
     LTK.require = function(id) {
         return LTK.exec(id);
-    }
+    };
 
     // public API
-    global.define   = LTK.define
+    global.define   = LTK.define;
 
 })(this);
